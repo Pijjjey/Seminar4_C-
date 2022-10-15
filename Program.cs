b57370da6bbc873c06971a7e53143b1641091c6a
Karina <nikiforovakarina45@gmail.com>
2022-10-16 00:54:39 +0300

    Added tasks

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 25
+/*Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Method(A,B);
+
+void Method (int x, int y)
+{
+    Console.Write(Math.Pow(x,y));
+}*/
+
+//Задача 27
+
+/*Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {N} равна {Sum(N)}");
+int Sum(int number)
+{
+    int result = 0;
+    int temp = 0;
+    while(number > 0)
+    {
+        temp = number % 10;
+        result = result + temp;
+        number = number/10;
+    }
+    return result;
+}*/
+
+
+//Задача 29
+/*int[] array = new int[8];
+GetBinaryArray(array.Length);
+Console.Write($"[{String.Join(",", Arr)}]");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for(int i = 0; i<size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}*/
